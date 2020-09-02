3efc246210e5bf9555e8469f63284e672f46cb7a
Pichuek <pi4eka@gmail.com>
2020-09-02 21:01:54 +0300

    Updates

---
+++ precache-manifest.ee3bb5ae116aa8acbbe3eb8466c28d4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d451a3fbe8aa242abc8674a02af1312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "419ca7befa9876dbeb26",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a23fdfa87ac2e9be4a7",
+    "url": "/static/js/2.f4d527d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f4d527d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419ca7befa9876dbeb26",
+    "url": "/static/js/main.3f67ed23.chunk.js"
+  },
+  {
+    "revision": "596af2c20d4de15a01f7",
+    "url": "/static/js/runtime-main.65cb6863.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
